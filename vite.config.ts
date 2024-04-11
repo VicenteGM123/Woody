b3e32fe6edc34fe35a08b5608c0dc638248be7c7
VicenteGM123 <vicentegarcia@uc.cl>
2024-04-11 18:57:19 -0400

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   // depending on your application, base can also be "/"
-  base: "Woody",
+  base: "/Woody/",
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
