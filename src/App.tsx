fc4c550129b114d35bf7e47391a815e16fc9a4c9
VicenteGM123 <vicentegarcia@uc.cl>
2024-04-11 20:25:09 -0400

    fix base path

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import "./App.css";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Train from "./pages/Train";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename="/Woody">
       <Routes>
-        <Route path="" element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="train" element={<Train />} />
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
