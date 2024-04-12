2f6b0b223407c609d8e750d6bedce3601892dc59
VicenteGM123 <vicentegarcia@uc.cl>
2024-04-11 20:39:34 -0400

    fix base path

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -41,31 +41,33 @@ const Header = () => {
               Iniciar sesión
             </button>
           )}
-          <button
-            data-collapse-toggle="navbar-sticky"
-            type="button"
-            onClick={toggleMenu} // Agrega el evento onClick
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-            aria-controls="navbar-sticky"
-            aria-expanded={menuOpen ? "true" : "false"} // Cambia el estado de aria-expanded en base al estado del menú
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
+          {!isLoading && isAuthenticated && (
+            <button
+              data-collapse-toggle="navbar-sticky"
+              type="button"
+              onClick={toggleMenu} // Agrega el evento onClick
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+              aria-controls="navbar-sticky"
+              aria-expanded={menuOpen ? "true" : "false"} // Cambia el estado de aria-expanded en base al estado del menú
             >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            </button>
+          )}
         </div>
         {/* Agrega la clase 'block' y el estilo 'display' en base al estado del menú */}
         {!isLoading && isAuthenticated && (
