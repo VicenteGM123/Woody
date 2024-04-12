fb922c9d55819bf48d04d9e4fd32885c27253628
VicenteGM123 <vicentegarcia@uc.cl>
2024-04-11 20:18:46 -0400

    fix base path

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="train" element={<Train />} />
         </Route>
